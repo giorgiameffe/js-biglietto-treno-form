1337529d3c7b2a15f9d6e5d55973acf1d0b23da6
giorgiameffe <giorgiameffe03@gmail.com>
2025-03-05 23:59:51 +0100

    feat: add milestone 2

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 const infoForm = document.querySelector('form');
 console.log(infoForm);
 
@@ -14,13 +15,22 @@ infoForm.addEventListener ('submit', function (event) {
     console.log(`I chilometri che l'utente vuole percorrere sono ${kmToDo}`);
 
     const myAge = parseInt(inputAge.value);
-    console.log(`L'età dell'utente è di ${myAge} anni`);
+    console.log(`L'età dell'utente è di ${myAge} anni`); 
 
-    console.log(finalPrice(kmToDo, myAge));
+    
+    // aggiungere elemento html
 
+    const ticketPrice = finalPrice(kmToDo, myAge);
+    const parentElement = document.querySelector('.container-final-price');
+    const childElement = document.createElement ('div');
+    parentElement.appendChild(childElement);
+    childElement.classList.add('final-price');
+    childElement.innerHTML =  ticketPrice;
+  
 })
 
-function finalPrice (km,age) {
+
+function finalPrice (km,age) { 
 
     const priceKm = 0.21;
 
@@ -47,7 +57,7 @@ function finalPrice (km,age) {
     // calcolare la cifra scontata
     fullPrice = fullPrice - discount;
         
-    // ridurre la cifra ottenuta ad un valore con massimo due decimali
+    //ridurre la cifra ottenuta ad un valore con massimo due decimali
     fullPrice.toFixed(2);
     console.log( `Il prezzo finale è di ${fullPrice.toFixed(2)} €`);
         
