ba300431e8935d592445d996d12ec221ff158c81
giorgiameffe <giorgiameffe03@gmail.com>
2025-03-05 18:43:14 +0100

    feat: complete milestone 1

---
+++ main.js
@@ -0,0 +1,57 @@
+const infoForm = document.querySelector('form');
+console.log(infoForm);
+
+const inputKilometres = document.getElementById('userKm');
+console.log(inputKilometres);
+
+const inputAge = document.getElementById('userAge');
+console.log(inputAge);
+
+infoForm.addEventListener ('submit', function (event) {
+    event.preventDefault();
+    
+    const kmToDo = parseInt(inputKilometres.value)
+    console.log(`I chilometri che l'utente vuole percorrere sono ${kmToDo}`);
+
+    const myAge = parseInt(inputAge.value);
+    console.log(`L'età dell'utente è di ${myAge} anni`);
+
+    console.log(finalPrice(kmToDo, myAge));
+
+})
+
+function finalPrice (km,age) {
+
+    const priceKm = 0.21;
+
+    //calcolo del prezzo base
+    let fullPrice = priceKm * km;
+
+    //calcolo della percentuale
+    let discountPercentage = 0;
+        
+    if (age < 18) {
+        
+        discountPercentage = 20;} 
+        
+        else if (age > 65) {
+    
+        discountPercentage = 40;
+    }
+    
+    console.log('Percentuale di sconto', discountPercentage);
+        
+    let discount = (fullPrice * discountPercentage) / 100;
+    console.log('Sconto', discount);
+        
+    // calcolare la cifra scontata
+    fullPrice = fullPrice - discount;
+        
+    // ridurre la cifra ottenuta ad un valore con massimo due decimali
+    fullPrice.toFixed(2);
+    console.log( `Il prezzo finale è di ${fullPrice.toFixed(2)} €`);
+        
+    return fullPrice;
+
+}
+
